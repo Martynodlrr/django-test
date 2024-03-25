16d200b62ed246ea19b2159489dbd707ea4a36fd
Martyn <martynodlrr@gmail.com>
2024-03-25 14:45:21 -0500

    added a index.html to redirect the user instead of a 404 page

--- django_test/settings.py
+++ django_test/settings.py
@@ -31,9 +31,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', 'localhost:8000']
+ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.1:8000', 'localhost:8000']
 
 
 # Application definition
--- django_test/urls.py
+++ django_test/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django_test.views import home_view
 
 urlpatterns = [
+    path('', home_view, name='home'),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
